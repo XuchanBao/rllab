4c89086cd24504fc3e1001325e1d96e5269145a8
XuchanBao <touran123@outlook.com>
2018-06-29 17:14:10 -0400

    Custom logger -- add clear_tabular() in logger.py

--- rllab/misc/logger.py
+++ rllab/misc/logger.py
@@ -346,3 +346,7 @@ def record_tabular_misc_stat(key, values, placement='back'):
         record_tabular(prefix + "Median" + suffix, np.nan)
         record_tabular(prefix + "Min" + suffix, np.nan)
         record_tabular(prefix + "Max" + suffix, np.nan)
+
+
+def clear_tabular():
+    del _tabular[:]
