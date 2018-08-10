973323308414304b99c3a4a74351c36070a9a16f
XuchanBao <touran123@outlook.com>
2018-08-10 15:06:54 +0800

    allow extra kwargs in VecEnvExcutor to make it compatible with mbrl

--- sandbox/rocky/tf/envs/vec_env_executor.py
+++ sandbox/rocky/tf/envs/vec_env_executor.py
@@ -6,14 +6,14 @@ from sandbox.rocky.tf.misc import tensor_utils
 
 
 class VecEnvExecutor(object):
-    def __init__(self, envs, max_path_length):
+    def __init__(self, envs, max_path_length, **kwargs):
         self.envs = envs
         self._action_space = envs[0].action_space
         self._observation_space = envs[0].observation_space
         self.ts = np.zeros(len(self.envs), dtype='int')
         self.max_path_length = max_path_length
 
-    def step(self, action_n):
+    def step(self, action_n, **kwargs):
         all_results = [env.step(a) for (a, env) in zip(action_n, self.envs)]
         obs, rewards, dones, env_infos = list(map(list, list(zip(*all_results))))
         dones = np.asarray(dones)
